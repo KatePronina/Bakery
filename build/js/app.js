4e901459b5ea6c686de2d991db00ef0d47de7dc2
Kate Pronina <kate-pronina-97@yandex.ru>
2018-07-15 20:11:37 +0300

    Сделала табы

--- build/js/app.js
+++ build/js/app.js
@@ -49,3 +49,27 @@ controls.addEventListener('click', onControlClick);
 setInterval(onButtonClick, 5000);
 
 })();
+(function () {
+
+var tabsHeaders = document.querySelectorAll('.members__item');
+var tabsBody = document.querySelectorAll('.member-description');
+
+var onTabsHeadersClick = function (evt) {
+	var dataTab = +(evt.currentTarget.getAttribute('data-tab'));
+	for (var i = 0; i < tabsHeaders.length; i++) {
+		if (dataTab === i) {
+			document.querySelector('.members__item--active').classList.remove('members__item--active');
+			tabsHeaders[i].classList.add('members__item--active');
+
+			document.querySelector('.member-description--active').classList.remove('member-description--active');
+			tabsBody[i].classList.add('member-description--active');
+		}
+	}
+}
+
+for (var i = 0; i < tabsHeaders.length; i++) {
+	tabsHeaders[i].addEventListener('click', onTabsHeadersClick);
+}
+
+
+})();
